738924873282b771bb881abef487c992e6846c66
Levente Zuck <zucklevente@gmail.com>
2021-06-11 13:41:20 +0200

    Change the title and add basic seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="hu">
 <head>
-    <title>zucklevente.hu</title>
+    <title>Zuck Levente - Szoftvertervező, programozó, mérnök.</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/f42cf818d6.js" crossorigin="anonymous"></script>
     <link href="css/app.css" rel="stylesheet">
+
+    <meta name="keywords" lang="hu" content="weboldalak, weboldal készítés, egyszerűen, webdesign, blog, képgaléria, honlapkészítés, weblap készítés, weboldal készítés budapest, budapest">
+    <meta name="description" content="Weboldalkészítés kisvállalatoknak, cégeknek, eseményekhez. Weblapok fejlesztése, keresőoptimalizálás, látogatottság elemezése és markeing megoldások.">
 </head>
 
 <body>
